7a9ac33964e2f03cdd8aac1a6167687e547a0d65
VitorDuarte303 <126691138+VitorDuarte303@users.noreply.github.com>
2023-03-10 11:11:30 +0000

    Adjust the Update Function

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class RubyController : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -13,6 +13,8 @@ public class NewBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 position = transform.position;
+        position.x = position.x + 0.1f;
+        transform.position = position;
     }
 }

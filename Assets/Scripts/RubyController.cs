4672692af19570de6c16f39c77342c4b82d3e100
VitorDuarte303 <126691138+VitorDuarte303@users.noreply.github.com>
2023-03-13 16:06:54 +0000

    Adjust the RubyController Script

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
-    public float speed = 3.0f;
+    public float speed = 0.3f;
 
     public int maxHealth = 5;
     int currentHealth;
+    
 
     Rigidbody2D rigidbody2d;
     float horizontal;
@@ -17,7 +18,10 @@ public class RubyController : MonoBehaviour
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
+
         currentHealth = maxHealth;
+
+        currentHealth = 1;
     }
 
     // Update is called once per frame
@@ -36,7 +40,7 @@ public class RubyController : MonoBehaviour
         rigidbody2d.MovePosition(position);
     }
 
-    void ChangeHealth(int amount)
+    public void ChangeHealth(int amount)
     {
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
         Debug.Log(currentHealth + "/" + maxHealth);

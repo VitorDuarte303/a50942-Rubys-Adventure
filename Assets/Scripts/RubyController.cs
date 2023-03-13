06f1611d6be05ced65707a4c1787bb4f8f60de68
VitorDuarte303 <126691138+VitorDuarte303@users.noreply.github.com>
2023-03-13 16:13:28 +0000

    Define a Property in RubyController

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -7,6 +7,8 @@ public class RubyController : MonoBehaviour
     public float speed = 0.3f;
 
     public int maxHealth = 5;
+
+    public int health { get { return currentHealth; } }
     int currentHealth;
     
 

dff010026f585dc7eb0160caee6dc0350fa642c4
VitorDuarte303 <126691138+VitorDuarte303@users.noreply.github.com>
2023-03-15 12:46:14 +0000

    What is Instantiate?

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -57,6 +57,11 @@ public class RubyController : MonoBehaviour
             if (invincibleTimer < 0)
                 isInvincible = false;
         }
+
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            Launch();
+        }
     }
 
     void FixedUpdate()
@@ -93,5 +98,6 @@ public class RubyController : MonoBehaviour
 
         animator.SetTrigger("Launch");
     }
+
 
 }

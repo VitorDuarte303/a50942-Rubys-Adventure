38b38a033e29b838cfbce085c3a97c1d148562d2
VitorDuarte303 <126691138+VitorDuarte303@users.noreply.github.com>
2023-03-16 21:44:31 +0000

    After adding an Audio Source to Ruby

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -24,6 +24,8 @@ public class RubyController : MonoBehaviour
     Animator animator;
     Vector2 lookDirection = new Vector2(1, 0);
 
+    AudioSource audioSource;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -31,6 +33,8 @@ public class RubyController : MonoBehaviour
         animator = GetComponent<Animator>();
 
         currentHealth = maxHealth;
+
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -110,6 +114,11 @@ public class RubyController : MonoBehaviour
         projectile.Launch(lookDirection, 300);
 
         animator.SetTrigger("Launch");
+    }
+
+    public void PlaySound(AudioClip clip)
+    {
+        audioSource.PlayOneShot(clip);
     }
 
 }

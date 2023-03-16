fdbdede23e77037224e432d98eb22735a8241510
VitorDuarte303 <126691138+VitorDuarte303@users.noreply.github.com>
2023-03-16 21:21:22 +0000

    Check Your Scripts

--- Assets/Scripts/NonPlayerCharacter.cs
+++ Assets/Scripts/NonPlayerCharacter.cs
@@ -4,19 +4,16 @@ using UnityEngine;
 
 public class NonPlayerCharacter : MonoBehaviour
 {
-
     public float displayTime = 4.0f;
     public GameObject dialogBox;
     float timerDisplay;
 
-    // Start is called before the first frame update
     void Start()
     {
         dialogBox.SetActive(false);
         timerDisplay = -1.0f;
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (timerDisplay >= 0)
